feb085d580334d5f41e40730adb77e87ec19bd57
josecanal-ebury <jose.canal@ebury.com>
2024-03-28 12:32:52 -0300

    [DOS-897] Add sensitive option to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "json" {
   description = "JSON encoded list of container definitions for use with other terraform resources such as aws_ecs_task_definition"
   value       = "[${local.json_map}]"
+  sensitive   = true
 }
 
 output "json_map" {
   description = "JSON encoded container definitions for use with other terraform resources such as aws_ecs_task_definition"
   value       = local.json_map
+  sensitive   = true
 }
